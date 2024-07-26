beb0abbd6ef9281db71510f5e6b48c8aaaaea282
alarv <alarvfm@gmail.com>
2024-07-26 17:07:32 +0300

    fix: keep original file from v1 branch

--- source/helpers/json_to_predreq.py
+++ source/helpers/json_to_predreq.py
@@ -1,51 +1,51 @@
-from tornado import httpserver
-from tornado import gen
-from tornado.ioloop import IOLoop
-import tornado.web
-from tornado.escape import json_decode, json_encode
 from ..entities.prediction_request import PredictionRequest
 from ..entities.dataset import Dataset
 from ..entities.dataentry import DataEntry
 from ..helpers import model_decoder
 import numpy as np
+from typing import Dict, Any
+
+# def decode(request):
+#     json_request = json_decode(request.body)
+#     pred_request = PredictionRequest(json_request['dataset'], json_request['rawModel'], json_request['additionalInfo'])
+#     input_series = pred_request.additionalInfo['fromUser']['inputSeries']
+#     independentFeatures = pred_request.additionalInfo['independentFeatures']
+#     shorted = []
+#     actualIndepFeatKeys = []
+#     # pred_request.dataset.features['key']
+#     for actual in input_series:
+#         for key in independentFeatures:
+#             if actual == independentFeatures[key]:
+#                 for feature in pred_request.dataset['features']:
+#                     if feature['name'] == actual:
+#                         shorted.append(feature['key'])
+#                 # shorted.append(key)
+#     dataEntryAll = []
+#     for dataEntry in pred_request.dataset['dataEntry']:
+#         dataEntryToInsert = []
+#         for key in shorted:
+#             dataEntryToInsert.append(dataEntry['values'][key])
+#         dataEntryAll.append(dataEntryToInsert)
+#     return dataEntryAll
 
 
-def decode(request):
-    json_request = json_decode(request.body)
-    pred_request = PredictionRequest(json_request['dataset'], json_request['rawModel'], json_request['additionalInfo'])
-
-    print("Dataset structure:")
-    print(pred_request.dataset)
-
-    input_series = pred_request.additionalInfo['fromUser']['inputSeries']
-    independentFeatures = pred_request.additionalInfo['independentFeatures']
+def decode(dataset, additionl_info):
+    input_series = additionl_info['fromUser']['inputSeries']
+    independentFeatures = additionl_info['independentFeatures']
     shorted = []
-
+    actualIndepFeatKeys = []
+    # pred_request.dataset.features['key']
     for actual in input_series:
         for key in independentFeatures:
             if actual == independentFeatures[key]:
-                for feature in pred_request.dataset['features']:
+                for feature in dataset['features']:
                     if feature['name'] == actual:
                         shorted.append(feature['key'])
-
-    print("Shorted keys:", shorted)
-
+                # shorted.append(key)
     dataEntryAll = []
-    dataEntry = pred_request.dataset['dataEntry']
-    print("DataEntry structure:", dataEntry)
-
-    if isinstance(dataEntry, dict) and 'values' in dataEntry:
-        values = dataEntry['values']
+    for dataEntry in dataset['dataEntry']:
         dataEntryToInsert = []
         for key in shorted:
-            print(f"Accessing key: {key}, type: {type(key)}")
-            value = values.get(str(key))
-            print(f"Retrieved value: {value}")
-            if value is not None:
-                dataEntryToInsert.append(value)
+            dataEntryToInsert.append(dataEntry['values'][key])
         dataEntryAll.append(dataEntryToInsert)
-    else:
-        print(f"Unexpected dataEntry structure: {dataEntry}")
-
-    print(f"DataEntryAll: {dataEntryAll}")
     return dataEntryAll
