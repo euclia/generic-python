eefcd71b10f1ea695fb984cd6b82ea6db6acc5f7
alarv <alarvfm@gmail.com>
2024-07-28 13:14:03 +0300

    fix: add healthcheck to 0.23

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+@app.get('/')
+def health_check():
+    return {'status': 'UP'}
 
 @app.post("/predict/sklearn/")
 # def sklearn_model(req: PredictionRequest):
